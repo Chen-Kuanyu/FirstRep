8b5db916d6c0c8409c583bb38b5fd71f5a7a5d89
rain <rain@DESKTOP-7SQF284>
2019-03-24 15:24:21 +0800

    FirstRep

---
+++ WebApplication1/Controllers/aspnet_SchemaVersionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class aspnet_SchemaVersionsController : Controller
+    {
+        private ASPNETDBEntities db = new ASPNETDBEntities();
+
+        // GET: aspnet_SchemaVersions
+        public ActionResult Index()
+        {
+            return View(db.aspnet_SchemaVersions.ToList());
+        }
+
+        // GET: aspnet_SchemaVersions/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aspnet_SchemaVersions aspnet_SchemaVersions = db.aspnet_SchemaVersions.Find(id);
+            if (aspnet_SchemaVersions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnet_SchemaVersions);
+        }
+
+        // GET: aspnet_SchemaVersions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: aspnet_SchemaVersions/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Feature,CompatibleSchemaVersion,IsCurrentVersion")] aspnet_SchemaVersions aspnet_SchemaVersions)
+        {
+            if (ModelState.IsValid)
+            {
+                db.aspnet_SchemaVersions.Add(aspnet_SchemaVersions);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aspnet_SchemaVersions);
+        }
+
+        // GET: aspnet_SchemaVersions/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aspnet_SchemaVersions aspnet_SchemaVersions = db.aspnet_SchemaVersions.Find(id);
+            if (aspnet_SchemaVersions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnet_SchemaVersions);
+        }
+
+        // POST: aspnet_SchemaVersions/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Feature,CompatibleSchemaVersion,IsCurrentVersion")] aspnet_SchemaVersions aspnet_SchemaVersions)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspnet_SchemaVersions).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspnet_SchemaVersions);
+        }
+
+        // GET: aspnet_SchemaVersions/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aspnet_SchemaVersions aspnet_SchemaVersions = db.aspnet_SchemaVersions.Find(id);
+            if (aspnet_SchemaVersions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnet_SchemaVersions);
+        }
+
+        // POST: aspnet_SchemaVersions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            aspnet_SchemaVersions aspnet_SchemaVersions = db.aspnet_SchemaVersions.Find(id);
+            db.aspnet_SchemaVersions.Remove(aspnet_SchemaVersions);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/aspnet_UsersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class aspnet_UsersController : Controller
+    {
+        private ASPNETDBEntities db = new ASPNETDBEntities();
+
+        // GET: aspnet_Users
+        public ActionResult Index()
+        {
+            var aspnet_Users = db.aspnet_Users.Include(a => a.aspnet_Applications).Include(a => a.aspnet_Membership).Include(a => a.aspnet_Profile);
+            return View(aspnet_Users.ToList());
+        }
+
+        // GET: aspnet_Users/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aspnet_Users aspnet_Users = db.aspnet_Users.Find(id);
+            if (aspnet_Users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnet_Users);
+        }
+
+        // GET: aspnet_Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName");
+            ViewBag.UserId = new SelectList(db.aspnet_Membership, "UserId", "Password");
+            ViewBag.UserId = new SelectList(db.aspnet_Profile, "UserId", "PropertyNames");
+            return View();
+        }
+
+        // POST: aspnet_Users/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ApplicationId,UserId,UserName,LoweredUserName,MobileAlias,IsAnonymous,LastActivityDate")] aspnet_Users aspnet_Users)
+        {
+            if (ModelState.IsValid)
+            {
+                aspnet_Users.UserId = Guid.NewGuid();
+                db.aspnet_Users.Add(aspnet_Users);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName", aspnet_Users.ApplicationId);
+            ViewBag.UserId = new SelectList(db.aspnet_Membership, "UserId", "Password", aspnet_Users.UserId);
+            ViewBag.UserId = new SelectList(db.aspnet_Profile, "UserId", "PropertyNames", aspnet_Users.UserId);
+            return View(aspnet_Users);
+        }
+
+        // GET: aspnet_Users/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aspnet_Users aspnet_Users = db.aspnet_Users.Find(id);
+            if (aspnet_Users == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName", aspnet_Users.ApplicationId);
+            ViewBag.UserId = new SelectList(db.aspnet_Membership, "UserId", "Password", aspnet_Users.UserId);
+            ViewBag.UserId = new SelectList(db.aspnet_Profile, "UserId", "PropertyNames", aspnet_Users.UserId);
+            return View(aspnet_Users);
+        }
+
+        // POST: aspnet_Users/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ApplicationId,UserId,UserName,LoweredUserName,MobileAlias,IsAnonymous,LastActivityDate")] aspnet_Users aspnet_Users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspnet_Users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName", aspnet_Users.ApplicationId);
+            ViewBag.UserId = new SelectList(db.aspnet_Membership, "UserId", "Password", aspnet_Users.UserId);
+            ViewBag.UserId = new SelectList(db.aspnet_Profile, "UserId", "PropertyNames", aspnet_Users.UserId);
+            return View(aspnet_Users);
+        }
+
+        // GET: aspnet_Users/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aspnet_Users aspnet_Users = db.aspnet_Users.Find(id);
+            if (aspnet_Users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnet_Users);
+        }
+
+        // POST: aspnet_Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            aspnet_Users aspnet_Users = db.aspnet_Users.Find(id);
+            db.aspnet_Users.Remove(aspnet_Users);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
